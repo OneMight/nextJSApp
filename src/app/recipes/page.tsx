9eb1a55f276ec0aeb0a3f573429c1657a0147e81
OneMight <dimamirankov63@gmail.com>
2026-01-23 14:46:50 +0300

    fix: padding

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="flex flex-row w-full">
       <NavSide />
-      <div className="flex flex-col justify-center items-center sm:justify-start sm:items-start p-2 sm:p-10 gap-10 w-full">
+      <div className="flex flex-col justify-center items-center sm:justify-start sm:items-start p-2 py-5 sm:p-10 gap-10 w-full">
         <div>
           <h1 className="font-bold text-2xl">Explore Recipes</h1>
           <p className="text-secondary-text text-md">
